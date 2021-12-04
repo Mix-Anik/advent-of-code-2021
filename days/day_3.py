cc79e5dcc750ac122cfdb466d2708de71d4a05fc
Mike <xxmixanikxx@gmail.com>
2021-12-04 12:04:12 +0200

    day 3 refactor

--- days/day_3.py
+++ days/day_3.py
@@ -2,47 +2,38 @@ from typing import List
 
 
 def part_1(data: List[str]):
-    row_length = len(data[0])
-    mcb = ''
-    lcb = ''
+    gamma_rate_bits = ''
 
-    for col_idx in range(row_length):
-        col_bits = [line[col_idx] for line in data]
-        col_mcb, col_lcb = calc_mcb_and_lcb(col_bits)
-        mcb += col_mcb
-        lcb += col_lcb
+    for col_idx in range(len(data[0])):
+        col_bits = [row[col_idx] for row in data]
+        gamma_rate_bits += find_most_common_bit(col_bits)
 
-    return int(lcb, 2) * int(mcb, 2)
+    gamma_rate = int(gamma_rate_bits, 2)
+    epsilon_rate = int(gamma_rate_bits.replace('1', 'x').replace('0', '1').replace('x', '0'), 2)
+
+    return gamma_rate * epsilon_rate
 
 
 def part_2(data: List[str]):
-    o2gr_data = co2sr_data = data
-    o2gr = co2sr = '-1'
+    o2_rating = find_rating(data)
+    co2_rating = find_rating(data, True)
 
-    for col_num in range(0, len(data[0])):
-        o2gr_bits = [line[col_num] for line in o2gr_data]
-        o2gr_bit, _ = calc_mcb_and_lcb(o2gr_bits)
-        o2gr_data = [line for line in o2gr_data if line[col_num] == o2gr_bit]
-
-        if len(o2gr_data) == 1:
-            o2gr = o2gr_data[0]
-
-        co2sr_bits = [line[col_num] for line in co2sr_data]
-        _, co2sr_bit = calc_mcb_and_lcb(co2sr_bits)
-        co2sr_data = [line for line in co2sr_data if line[col_num] == co2sr_bit]
-
-        if len(co2sr_data) == 1:
-            co2sr = co2sr_data[0]
-
-    return int(co2sr, 2) * int(o2gr, 2)
+    return o2_rating * co2_rating
 
 
-def calc_mcb_and_lcb(bits: List[str]):
-    # calculate most / least common bits
-    bits_sum = sum([int(bit) for bit in bits])
-    bits_hl = len(bits) / 2
+def find_most_common_bit(bits: List[str], least=False):
+    mcb_boolean = sum([int(bit) for bit in bits]) >= len(bits) / 2
 
-    if bits_sum >= bits_hl:
-        return '1', '0'
-    else:
-        return '0', '1'
+    return str(int(not mcb_boolean if least else mcb_boolean))
+
+
+def find_rating(data: List[str], use_lcb=False):
+    col_idx = 0
+
+    while len(data) > 1:
+        bits = [row[col_idx] for row in data]
+        common_bit = find_most_common_bit(bits, use_lcb)
+        data = [row for row in data if row[col_idx] == common_bit]
+        col_idx += 1
+
+    return int(data[0], 2)
